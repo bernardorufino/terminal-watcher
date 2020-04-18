7795f65d405f94425313a0f68e1b64d4bd727ad0
Bernardo Rufino <brufino@google.com>
2020-04-19 00:25:17 +0100

    Remove env

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,4 +7,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    print(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
     return 'Hello, World!'
